6ad0d9ce39010aef349c3ff4d2647d35d1d0cf1f
YSRKEN <ysr.ken@gmail.com>
2020-11-05 11:35:45 +0900

    [add] 切り替え式ボタンを作成した

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 const App: React.FC = () => {
+  const [typeStatusList, setTypeStatusList] = useState<boolean[]>([false, false, false, false, false]);
+
+  const flipTypeStatus = (index: number) => {
+    const newList = [...typeStatusList];
+    newList[index] = !newList[index];
+    setTypeStatusList(newList);
+  };
+
   return (
     <Container>
       <Row className="my-3">
         <Col className="text-center">
           <h1>あいミス属性チェッカー</h1>
+        </Col>
+      </Row>
+      <Row className="my-3">
+        <Col className="text-center">
+          <Form>
+            <Form.Group>
+              <Button variant={typeStatusList[0] ? "danger" : "outline-danger"} className="mr-3" size="lg"
+                onClick={() => flipTypeStatus(0)}>力</Button>
+              <Button variant={typeStatusList[1] ? "success" : "outline-success"} className="mr-3" size="lg"
+                onClick={() => flipTypeStatus(1)}>芸</Button>
+              <Button variant={typeStatusList[2] ? "primary" : "outline-primary"} className="mr-3" size="lg"
+                onClick={() => flipTypeStatus(2)}>知</Button>
+              <Button variant={typeStatusList[3] ? "warning" : "outline-warning"} className="mr-3" size="lg"
+                onClick={() => flipTypeStatus(3)}>理</Button>
+              <Button variant={typeStatusList[4] ? "secondary" : "outline-secondary"} size="lg"
+                onClick={() => flipTypeStatus(4)}>心</Button>
+            </Form.Group>
+          </Form>
         </Col>
       </Row>
     </Container>
